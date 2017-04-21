b3e1a0ff2fc6e7308bf4ab123a2017454ad1ca94
francescaangelini <francesca.angelini18@gmail.com>
2017-04-21 15:28:12 +0200

    aggiornata class contact

--- src/it/overnet/models/Contact.java
+++ src/it/overnet/models/Contact.java
@@ -2,4 +2,41 @@ package it.overnet.models;
 
 public class Contact {
 
+	int id;
+	String nome;
+	String cognome;
+	String tel;
+	String mail;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCognome() {
+		return cognome;
+	}
+	public void setCognome(String cognome) {
+		this.cognome = cognome;
+	}
+	public String getTel() {
+		return tel;
+	}
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+	public String getMail() {
+		return mail;
+	}
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+	 
+	
 }
