24c6b6fd2b6459baf8ffce06a0a2eeeef4b4b7f0
liviom87 <liviom87@hotmail.it>
2017-04-21 15:54:09 +0200

    ho modificato login aggiunto logout lota!!!

--- src/it/overnet/servlet/Login.java
+++ src/it/overnet/servlet/Login.java
@@ -1,6 +1,7 @@
 package it.overnet.servlet;
 
 import java.io.IOException;
+import java.util.logging.Logger;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -8,13 +9,28 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+
 @WebServlet("/Login")
 public class Login extends HttpServlet {
+	
+	private static final Logger logger = Logger.getLogger(Login.class.getName());
+
 	private static final long serialVersionUID = 1L;
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+
+		if (username.equals("user") && password.equals("pass")) {
+			response.sendRedirect("success.html");
+
+			logger.info("Allow access to" + username);
+		} else {
+			response.sendRedirect("error.html");
+			logger.warning("Access denied");
+		}
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
