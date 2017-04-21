cdaecbf85bd33308069cb8b0df69d6e3d905589c
CORSO <CORSO@DESKTOP-MQ75U6U>
2017-04-21 15:56:28 +0200

    ooooooooooooo

--- src/it/overnet/servlet/Login.java
+++ src/it/overnet/servlet/Login.java
@@ -1,6 +1,7 @@
 package it.overnet.servlet;
 
 import java.io.IOException;
+import java.util.logging.Logger;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -8,13 +9,20 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+//import it.overnet.LoginFilter;
+
 @WebServlet("/Login")
 public class Login extends HttpServlet {
+	
+	
 	private static final long serialVersionUID = 1L;
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+		
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
