3886b1434ce774385049af9f3b0f13d55d127b93
CORSO <CORSO@DESKTOP-MQ75U6U>
2017-05-02 15:28:06 +0200

    aggiunta del metodo delete e servlet aggiustata

--- src/it/overnet/utilities/Constant.java
+++ src/it/overnet/utilities/Constant.java
@@ -5,7 +5,7 @@ public class Constant {
 	public static final String DB_DRIVER = "oracle.jdbc.driver.OracleDriver"; // driver database usato
 
 	//al posto di "xe" settare il nome del vostro database
-	public static final String DB_CONNECTION = "jdbc:oracle:thin:@localhost:1521:JDBC"; // la macchina su cui gira
+	public static final String DB_CONNECTION = "jdbc:oracle:thin:@localhost:1521:MyContacts"; // la macchina su cui gira
 
 	public static final String DB_USERNAME = "system";
 
